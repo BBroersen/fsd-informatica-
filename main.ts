16bf7eed64e559aca3e28c9e3a9e2a0894a9a837
BBroersen <112395027+BBroersen@users.noreply.github.com>
2023-03-13 11:22:07 +0100

    bb

--- main.ts
+++ main.ts
@@ -1,14 +1,60 @@
 let Item = false
+music.setVolume(150)
+music.playTone(131, music.beat(BeatFraction.Whole))
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+music.playTone(139, music.beat(BeatFraction.Whole))
+basic.clearScreen()
+basic.showIcon(IconNames.SmallDiamond)
+basic.pause(100)
+music.playTone(147, music.beat(BeatFraction.Whole))
+basic.clearScreen()
+basic.showIcon(IconNames.Diamond)
+basic.pause(100)
+music.playTone(156, music.beat(BeatFraction.Whole))
+basic.clearScreen()
+basic.showLeds(`
+    . # . # .
+    # . . . #
+    . . . . .
+    # . . . #
+    . # . # .
+    `)
+basic.pause(100)
+music.playTone(165, music.beat(BeatFraction.Whole))
+basic.clearScreen()
+basic.showLeds(`
+    # . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . #
+    `)
+basic.pause(100)
+music.playTone(175, music.beat(BeatFraction.Whole))
+basic.clearScreen()
+basic.pause(100)
+music.setVolume(60)
+music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1, 3721, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+basic.showString("ON")
+basic.pause(1000)
+music.setVolume(255)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 17 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) >= 10 && maqueen.Ultrasonic(PingUnit.Centimeters) <= 12 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         Item = Math.randomBoolean()
         if (Item == true) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 155)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             basic.pause(300)
         }
         if (Item == false) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 155)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             basic.pause(300)
         }
@@ -17,15 +63,15 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 12 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) >= 8 && maqueen.Ultrasonic(PingUnit.Centimeters) < 10 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         Item = Math.randomBoolean()
         if (Item == true) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 125)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 55)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             basic.pause(300)
         }
         if (Item == false) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 125)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 55)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             basic.pause(300)
         }
@@ -34,7 +80,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 7 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 8 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         Item = Math.randomBoolean()
         if (Item == true) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
@@ -51,14 +97,22 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    Item = Math.randomBoolean()
+    while (Item == true) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        basic.pause(100)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    }
     while (Item == false) {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         basic.pause(100)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
-    while (Item == true) {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        basic.pause(100)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+})
+basic.forever(function () {
+    Item = Math.randomBoolean()
+    while (Item != (true || false)) {
+        music.setVolume(50)
+        music.playMelody("C5 A - B E - G B ", 120)
     }
 })
